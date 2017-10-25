7596b5b16a3e3e8f12457a94c77b2cdd6a658031
jeffvippan <www.jeffpan@gmail.com>
2017-10-25 10:37:17 +0800

    1.配置Eureka 2.配置config-server

---
+++ config-server/src/main/java/com/holoworld/configserver/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.holoworld.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
